6a4ce6f67a50b70a0da4eb062ffa22955209bc01
Krrish Taneja <krrishtaneja8@gmail.com>
2026-01-26 09:20:00 +0530

    feat(backend): connect to MongoDB using mongoose

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// Establishes MongoDB connection on server startup
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
